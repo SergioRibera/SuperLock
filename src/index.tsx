87b6178259336a3b0ed8f496b030de7cf283e5ee
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2021-11-27 04:37:51 -0400

    Feature(Implement Default Theme): Implement default theme into screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { DefaultTheme } from './Screens/DefaultTheme';
 
 ReactDOM.render(
     <React.StrictMode>
+        <DefaultTheme options={{ showHours: true, showMinutes: true, showSeconds: false, showDate: true, showAmPm: false }} />
     </React.StrictMode>,
     document.getElementById('root')
 );
